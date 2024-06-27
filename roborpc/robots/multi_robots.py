6fd6d5689cffa274f9a58eded63bb73fcba84e15
WangYong-999 <2839297390@qq.com>
2024-06-27 21:43:35 +0800

    feat: update data collection

--- roborpc/robots/multi_robots.py
+++ roborpc/robots/multi_robots.py
@@ -131,25 +131,5 @@ class MultiRobots(RobotBase):
         return self.ee_poses
 
 
-if __name__ == '__main__':
-    import zerorpc
-    multi_realman = MultiRobots()
-    multi_realman.connect_now()
-    print(multi_realman.get_robot_ids())
-    print(multi_realman.get_robot_state())
-
-    multi_realman.set_joints({"realman_1": [0.0, 0.0, -3.1415926/2, -3.1415926/2,
-                              0.0, 0.0, 0.0]},
-                             action_space={"realman_1": "joint_position"}, blocking={"realman_1": True})
-    # multi_realman.set_gripper(
-    #     {"realman_1": [0.1]}, action_space={"realman_1": "gripper_position"}, blocking={"realman_1": True})
-
-    # multi_robots = MultiRobots()
-    # s = zerorpc.Server(multi_robots)
-    # rpc_port = multi_robots.robot_config['sever_rpc_ports'][0]
-    # print(f"RPC Port: {rpc_port}")
-    # logger.info(f"RPC Server Start on {rpc_port}")
-    # s.bind(f"tcp://0.0.0.0:{rpc_port}")
-    # s.run()
 
 
